eac2631cba50a694f48a71579103afd57703f355
Alec Blakeley <alecblkly@gmail.com>
2020-02-11 13:11:30 -0500

    Solution for file_io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,11 +9,31 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
+with open('foo.txt', 'r') as f:
+    print(f.read())
+
+if f.closed == True:
+    print("-----------------------")
+    print("File 'foo.txt' has been closed.")
+else:
+    print("-----------------------")
+    print("File 'foo.txt' remains open.")
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open("bar.txt", "w") as f1:
+    f1.write("words, words, words\n")
+    f1.write("-------------------\n")
+    f1.write("Llamas are neat.\n")
+    f1.write("Hello, world!")
+
+if f1.closed == True:
+    print("-----------------------")
+    print("File 'bar.txt' has been successfully written and has been closed.")
+else:
+    print("-----------------------")
+    print("File 'bar.txt' was not successfully written and remains open.")
